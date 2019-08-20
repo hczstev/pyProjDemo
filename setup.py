561b090f368d53a82dc2ff00edfdee35fc1d4012
huangchengzhang <huangchengzhang@oceandark.com>
2019-08-20 09:06:42 +0800

    Adding test skeleton

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="PyProjDemo",
     version="0.0.0",
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     description="Python project demo",
     long_description="It is a python project demo",
     install_description=[],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
     # Other useful parameters: py_modules, cmdclass, include_package_data...
 )
